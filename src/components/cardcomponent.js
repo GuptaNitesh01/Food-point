7001fca6fcb355ccac6c7430efb5722e4313e8cb
GuptaNItesh01 <guptanitesh0102@gmail.com>
2024-05-25 12:51:51 +0530

    dummychanges

--- src/components/cardcomponent.js
+++ src/components/cardcomponent.js
@@ -1,21 +1,79 @@
 import Restaurantcard from "./restaurentcard";
 
 const Cardcontainer = () => {
+  const restaurantdata = [
+    {
+      imgUrl : "https://tse2.mm.bing.net/th?id=OIP.dHqpahhit7fjvYZZuWwVyQHaE7&pid=Api&P=0&h=180",
+      title : "Prep Fitness kitchen",
+      starrating : "4.5",
+      deliveryTime : "40-50",
+      cuisines : "Healthy and Fresh Food",
+      location : "Bandra West",
+    },
+    {
+      imgUrl :"https://tse4.mm.bing.net/th?id=OIP.uqYdpk_sNpTveBnnzn5AjwHaEY&pid=Api&P=0&h=180",
+      title : "Radha Krishna",
+      starrating : "4.9",
+      deliveryTime : "30-35",
+      cuisines : "South Indian, Asian, Tibetan, Chinese",
+      location : "Ghatkopar West",
+    },
+    {
+      imgUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qrK7sIJh2VaanAlPe1eB89Z68bK9ZUMleA&usqp=CAU",
+      title : "american dhaba",
+      starrating :"5",
+      deliveryTime : "30-35",
+      cuisines : "America, western, Indian",
+      location: "tank road"
+    },
+    {
+      imgUrl :"https://i.ytimg.com/vi/6E0QFJ3g1Yw/maxresdefault.jpg",
+      title : "Aapla Dhaba",
+      starrating : "4.9",
+      deliveryTime : "30-35",
+      cuisines : "South Indian, Asian, Tibetan, Chinese",
+      location : "Ghatkopar West",
+    },
+    {
+      imgUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qrK7sIJh2VaanAlPe1eB89Z68bK9ZUMleA&usqp=CAU",
+      title : "american dhaba",
+      starrating :"5",
+      deliveryTime : "30-35",
+      cuisines : "America, western, Indian",
+      location: "tank road",
+      },
+      {
+        imgUrl : "https://tse4.mm.bing.net/th?id=OIP.isKvZO4fnOOR-Hi3CbdEKQHaHa&pid=Api&P=0&h=180",
+        title : "Kubec BistrocBar",
+        starrating : "4.5",
+        deliveryTime : "40-45",
+        cuisines : "Chinese, Asian, Tibetan, Desserts",
+        location : "Powai",
+      },
+    
+
+
+  ];
     return(
       <div className="container d-flex flex-wrap gap-5"> 
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-      <Restaurantcard/>
-     
+      {
+        restaurantdata.map((restaurant) => {
+          return(
+            <Restaurantcard
+            {...restaurant}
+              // imgurl={restaurant.imgUrl}
+              // title={restaurant.title}
+              // starrating={restaurant.starrating}
+              // deliverytime={restaurant.deliveryTime}
+              // cuisines={restaurant.cuisines}
+              // location={restaurant.location}
+              />
+
+          );
+        })
+      }
       </div>
-    )
-  }
+    );
+  };
 
   export default Cardcontainer;
--- src/components/restaurentcard.js
+++ src/components/restaurentcard.js
@@ -1,17 +1,26 @@
-const Restaurantcard = () => {
+const Restaurantcard = (props) => {
+  // imgurl, title , starrating , deliverytime , cuisines , location
     return(
       <div className="custom-card">
       <div className="mb-2 ">
-        <img src="https://tse2.mm.bing.net/th?id=OIP.dHqpahhit7fjvYZZuWwVyQHaE7&pid=Api&P=0&h=180" width="100%"/> 
+        <img 
+        src={props.imgUrl}
+        // src="https://tse2.mm.bing.net/th?id=OIP.dHqpahhit7fjvYZZuWwVyQHaE7&pid=Api&P=0&h=180" width="100%"
+        /> 
         </div>
         <div className="px-2">
-        <h2> Prep Fitness kitchen </h2>
+        <h2> {props.title} </h2>
+        {/* <h2> Prep Fitness kitchen </h2> */}
         <div className="d-flex justify-content-between">
-          <div>⭐4.5</div>
-          <div> 40 - 50 min</div>
+          <div>⭐{props.starrating}</div>
+          {/* <div>⭐4.5</div> */}
+          <div> {props.deliverytime} min </div>
+          {/* <div> 40 - 50 min</div> */}
         </div>
-        <div className="text-secondary">Healthy and Fresh Food </div>
-        <div>Bandra West</div>
+        <div className="text-secondary"> {props.cuisines}</div>
+        {/* <div className="text-secondary">Healthy and Fresh Food </div> */}
+        <div>{props.location}</div>
+        {/* <div>Bandra West</div> */}
         </div>
       </div>
     )
