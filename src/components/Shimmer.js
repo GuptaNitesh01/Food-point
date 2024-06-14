23a31d6a443ca75b7bbc96d0a03459920ea846f6
GuptaNItesh01 <guptanitesh0102@gmail.com>
2024-06-14 12:22:58 +0530

    shimmering

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,7 +1,22 @@
-// const Shimmer = () => {
-//     return(
+const Shimmer = () => {
+    return new Array(20).fill(0).map(() => (
+        <div className="custom-card mb-4">
+          <div className="image">
+            <div className="shimmer-img"></div>
+          </div>
+          <div>
+          <div className="content">
+              <h4 className="shimmerline mb-2"></h4>
+              <div className="d-flex mb-2 justify-content-between">
+                <span className="shimmerline" style={{width:"50px"}}></span> 
+                <span className="shimmerline" style={{width:"50px"}}></span>
+                </div>
+              <div className="cuisines shimmerline mb-2"></div>
+              <p className="shimmerline mb-2"></p>
+          </div>
+      </div>  
+      </div>
+ )); 
+};
 
-//     )
-// }
-
-// export default Shimmer;
+export default Shimmer;
--- src/components/cardcomponent.js
+++ src/components/cardcomponent.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const Cardcomponent = () => {
   const[count,setCount] = useState(0);
   const [restaurantData,setRestaurantData] = useState([]);
+  const [loading,setLoading] = useState(true)
   const [restaurantCollection,setRestaurantCollection] = useState([]);
   const [searchtext,setSearchText] = useState("");
   console.log("restaurantList",restaurantData);
@@ -25,6 +26,7 @@ const Cardcomponent = () => {
       const getRestaurants = async()  =>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
+        setLoading(false);
         console.log("json",json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setRestaurantData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setRestaurantCollection(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
@@ -34,6 +36,15 @@ const Cardcomponent = () => {
     },[])
 
     console.log("Component is render ");
+
+
+    if(loading){
+      return (
+        <div className="container d-flex flex-wrap gap-4">
+      <shimmer/>
+      </div>
+      )
+    }
 
   return (
     <div>
